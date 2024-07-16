73ae56023ccf9921508324d5d30d52d9d2e5650c
AndySmithQA <andy.smith@qa.com>
2024-07-16 10:02:05 +0100

    models added

---
+++ src/ts/models/bootcamp.ts
@@ -0,0 +1,11 @@
+import  { DataResource} from '../services/DataResource';
+
+export interface BootcampProps {
+    title: string
+    description: string
+    courses: string[]
+    length: number
+}
+
+export const Bootcamp = new DataResource<BootcampProps>('http://localhost:3000/bootcamps')
+
